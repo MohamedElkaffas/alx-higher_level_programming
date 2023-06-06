9326f201a8b0322fc8ce7de0921275b75147b993
Your Name <you@example.com>
2023-06-06 07:02:46 -0700

    c

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for c in range(97, 123):
-    if c != 101 and ch != 113:
-        print("{:c}".format(c), end='')
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
